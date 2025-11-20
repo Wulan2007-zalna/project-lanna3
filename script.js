7390dc469cb7cf95f3d08d78d97df5c44e3244c0
Wulan2007-zalna <wulannuraini2804@gmail.com>
2025-11-20 07:29:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const btn = document.getElementById("btn");
-const msg = document.getElementById("message");
+const message = document.getElementById("message");
 
+const facts = [
+    "Gunung Semeru adalah gunung tertinggi di Jawa!",
+    "Gunung Bromo memiliki kawah aktif dengan diameter 800 meter.",
+    "Kawah Ijen terkenal dengan api biru yang langka di dunia.",
+    "Gunung Arjuno memiliki jalur pendakian yang sangat menantang.",
+];
 
 btn.addEventListener("click", () => {
-msg.textContent = "Tombol sudah diklik! 🔥";
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    message.textContent = randomFact;
 });
