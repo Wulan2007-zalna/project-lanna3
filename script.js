af3753c3429da078b120c2661daeeac1cd9b27cc
Wulan2007-zalna <wulannuraini2804@gmail.com>
2025-11-20 07:16:00 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const msg = document.getElementById("message");
+
+
+btn.addEventListener("click", () => {
+msg.textContent = "Tombol sudah diklik! 🔥";
+});
